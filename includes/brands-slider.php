c021f05d126ce775c6b933ac8788af1072bb5af8
Nguyenhoanglongg <long94079@gmail.com>
2022-11-27 09:53:13 +0700

    Commit code 27/11/22 by HoangLong

--- includes/brands-slider.php
+++ includes/brands-slider.php
@@ -1,85 +1,84 @@
 <div id="brands-carousel" class="logo-slider wow fadeInUp">
-	<h3 class="section-title">Our Brands</h3>
+	<h3 class="section-title">Popular Authors</h3>
 	<div class="logo-slider-inner">
 		<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/aoc.jpg" src="assets/images/blank.gif" alt="">
+				<a href="https://www.amazon.com/Michelle-Obama/e/B07B436TLF/ref=s9_acss_bw_cg_CFQ422_2a1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-15&pf_rd_r=7Q38YCTH1SYRZRDCRKW0&pf_rd_t=101&pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&pf_rd_i=283155" class="image">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q2-20/Michelle_Obama.jpg" alt="">
 				</a>
 			</div>
 			<!--/.item-->
 
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/bajaj.jpg" src="assets/images/blank.gif" alt="">
+				<a href="https://www.amazon.com/gp/product/B09D3GZ8JP/ref=s9_acss_bw_cg_CFQ422_2b1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-15&pf_rd_r=7Q38YCTH1SYRZRDCRKW0&pf_rd_t=101&pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&pf_rd_i=283155" class="image">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q3-2022/Lore_Olympus.jpg" alt="">
 				</a>
 			</div>
 			<!--/.item-->
 
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/blackberry.jpg" src="assets/images/blank.gif" alt="">
+				<a href="https://www.amazon.com/Mark-Bittman/e/B000APUJB0/ref=s9_acss_bw_cg_CFQ422_2c1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-15&pf_rd_r=7Q38YCTH1SYRZRDCRKW0&pf_rd_t=101&pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&pf_rd_i=283155" class="image">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q4-2022/Mark_Bittman.jpg" alt="Mark Bittman">
 				</a>
 			</div>
 			<!--/.item-->
 
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/canon.jpg" src="assets/images/blank.gif" alt="">
+				<a href="https://www.amazon.com/Ina-Garten/e/B001H6PWM2/ref=s9_acss_bw_cg_CFQ422_2d1_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-15&pf_rd_r=7Q38YCTH1SYRZRDCRKW0&pf_rd_t=101&pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&pf_rd_i=283155" class="image">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q4-2022/ina_garten.jpg" alt="Ina Garten"> </a>
+			</div>
+
+			<div class="item">
+				<a href="/Americas-Test-Kitchen/e/B004LSANTK/ref=s9_acss_bw_cg_CFQ422_2e1_w?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-15&amp;pf_rd_r=7Q38YCTH1SYRZRDCRKW0&amp;pf_rd_t=101&amp;pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&amp;pf_rd_i=283155" aria-label="America's Test Kitchen">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q4-2020/Americas_Test_Kitchen.jpg" alt="America's Test Kitchen">
 				</a>
 			</div>
 
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/compas.jpg" src="assets/images/blank.gif" alt="">
+				<a href="/Marie-Kondo/e/B00J59XZJA/ref=s9_acss_bw_cg_CFQ422_3a1_w?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-15&amp;pf_rd_r=7Q38YCTH1SYRZRDCRKW0&amp;pf_rd_t=101&amp;pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&amp;pf_rd_i=283155" aria-label="Marie Kondo">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q4-2022/Marie_Kondo.jpg" alt="Marie Kondo">
 				</a>
 			</div>
 
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/daikin.jpg" src="assets/images/blank.gif" alt="">
-				</a>
-			</div>
-
-			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/dell.jpg" src="assets/images/blank.gif" alt="">
+				<a href="/Danielle-Steel/e/B000APK2GC/ref=s9_acss_bw_cg_CFQ422_3c1_w?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-15&amp;pf_rd_r=7Q38YCTH1SYRZRDCRKW0&amp;pf_rd_t=101&amp;pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&amp;pf_rd_i=283155" aria-label="Danielle Steel">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q4-2021/1984821520.jpg" alt="Danielle Steel">
 				</a>
 			</div>
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/samsung.jpg" src="assets/images/blank.gif" alt="">
+				<a href="/Nicholas-Sparks/e/B000APGF36/ref=s9_acss_bw_cg_CFQ422_3d1_w?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-15&amp;pf_rd_r=7Q38YCTH1SYRZRDCRKW0&amp;pf_rd_t=101&amp;pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&amp;pf_rd_i=283155" aria-label="Nicholas Sparks">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q4-2022/Nicholas_Sparks.jpg" alt="Nicholas Sparks">
 				</a>
 			</div>
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/hcl.jpg" src="assets/images/blank.gif" alt="">
+				<a href="/John-Grisham/e/B000AQ40M8/ref=s9_acss_bw_cg_CFQ422_4b1_w?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-15&amp;pf_rd_r=7Q38YCTH1SYRZRDCRKW0&amp;pf_rd_t=101&amp;pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&amp;pf_rd_i=283155" aria-label="John Grisham">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2019/img/Books/CatFav/Q4-19/John_Grisham.jpg" alt="John Grisham">
 				</a>
 			</div>
 
 
 
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/sony.jpg" src="assets/images/blank.gif" alt="">
+				<a href="/George-R-R-Martin/e/B000APIGH4/ref=s9_acss_bw_cg_CFQ422_5a1_w?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-15&amp;pf_rd_r=7Q38YCTH1SYRZRDCRKW0&amp;pf_rd_t=101&amp;pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&amp;pf_rd_i=283155" aria-label="George R. R. Martin">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q3-2022/George_RR_MARTIN.jpg" alt="George R. R. Martin">
 				</a>
 			</div>
 
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/voltas.jpg" src="assets/images/blank.gif" alt="">
+				<a href="/George-R-R-Martin/e/B000APIGH4/ref=s9_acss_bw_cg_CFQ422_5a1_w?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-15&amp;pf_rd_r=7Q38YCTH1SYRZRDCRKW0&amp;pf_rd_t=101&amp;pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&amp;pf_rd_i=283155" aria-label="George R. R. Martin">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q3-2022/George_RR_MARTIN.jpg" alt="George R. R. Martin">
 				</a>
 			</div>
 
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/lg.jpg" src="assets/images/blank.gif" alt="">
+				<a href="/Adam-Silvera/e/B00O7FYR58/ref=s9_acss_bw_cg_CFQ422_5b1_w?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-15&amp;pf_rd_r=7Q38YCTH1SYRZRDCRKW0&amp;pf_rd_t=101&amp;pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&amp;pf_rd_i=283155" aria-label="Adam Silvera">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q4-2022/Adam_Silvera.jpg" alt="Adam Silvera">
 				</a>
 			</div>
 
 			<div class="item">
-				<a href="#" class="image">
-					<img data-echo="brandsimage/lenovo.jpg" src="assets/images/blank.gif" alt="">
+				<a href="/Adam-Silvera/e/B00O7FYR58/ref=s9_acss_bw_cg_CFQ422_5b1_w?pf_rd_m=ATVPDKIKX0DER&amp;pf_rd_s=merchandised-search-15&amp;pf_rd_r=7Q38YCTH1SYRZRDCRKW0&amp;pf_rd_t=101&amp;pf_rd_p=776ce935-6a5f-443d-8bc6-c4047d6e341c&amp;pf_rd_i=283155" aria-label="Adam Silvera">
+					<img src="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Books/CatFav/Q4-2022/Adam_Silvera.jpg" alt="Adam Silvera">
 				</a>
 			</div>
 
--- pagination.php
+++ pagination.php
@@ -1,35 +1,39 @@
-<?php
-if ($current_page > 3) {
-    $first_page = 1; ?>
-    <li class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $first_page ?>">First</li>
-<?php }
-if ($current_page > 1) {
-    $prev_page = $current_page - 1;
-?>
-    <li class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $prev_page ?>">Prev</li>
-<?php }
-?>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="admin_style.css">
+<div id="pagination">
+    <?php
+    if ($current_page > 3) {
+        $first_page = 1; ?>
+        <a class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $first_page ?>">First</a>
+    <?php }
+    if ($current_page > 1) {
+        $prev_page = $current_page - 1;
+    ?>
+        <a class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $prev_page ?>">Prev</a>
+    <?php }
+    ?>
 
 
-<?php for ($num = 1; $num <= $totalPages; $num++) { ?>
-    <?php if ($num != $current_page) { ?>
-        <?php if ($num > $current_page - 3 && $num < $current_page + 3) { ?>
-            <li class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $num ?>"><?= $num ?></li>
+    <?php for ($num_page = 1; $num_page <= $totalPages; $num_page++) { ?>
+        <?php if ($num_page != $current_page) { ?>
+            <?php if ($num_page > $current_page - 3 && $num_page < $current_page + 3) { ?>
+                <a class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $num_page ?>"><?= $num_page ?></a>
+            <?php } ?>
+        <?php } else { ?>
+            <strong class="current-page page-item"><?= $num_page ?></strong>
         <?php } ?>
-    <?php } else { ?>
-        <strong class="current-page page-item"><?= $num ?></strong>
     <?php } ?>
-<?php } ?>
-<?php
-if ($current_page < $totalPages - 1) {
-    $next_page = $current_page + 1; ?>
-    <li class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $next_page ?>">Next</li>
-<?php
-}
-if ($current_page < $totalPages - 3) {
-    $end_page = $totalPages; ?>
-    <li class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $end_page ?>">Last</li>
+    <?php
+    if ($current_page < $totalPages - 1) {
+        $next_page = $current_page + 1; ?>
+        <a class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $next_page ?>">Next</a>
+    <?php
+    }
+    if ($current_page < $totalPages - 3) {
+        $end_page = $totalPages; ?>
+        <a class="page-item" href="?per_page=<?= $item_per_page ?>&page=<?= $end_page ?>">Last</a>
 
-<?php
-}
-?>
+    <?php
+    }
+    ?>
+</div>
